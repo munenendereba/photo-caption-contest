d7409bf0a90eff274e01e5a75f7e2f753b174b79
Isaac Munene Ndereba <38168136+munenendereba@users.noreply.github.com>
2023-10-23 22:40:16 +0300

    added db configs

---
+++ db/config/config.cjs
@@ -0,0 +1,16 @@
+const Sequelize = require("sequelize");
+const configDotenv = require("dotenv").config();
+
+const { DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASSWORD, DB_DIALECT } =
+  process.env;
+
+module.exports = {
+  development: {
+    username: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    host: DB_HOST,
+    port: DB_PORT,
+    dialect: DB_DIALECT,
+  },
+};
---
+++ db/config/connection.js
@@ -0,0 +1,37 @@
+import { configDotenv } from "dotenv";
+import { Sequelize } from "sequelize";
+
+configDotenv();
+
+const { DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASSWORD, DB_DIALECT } =
+  process.env;
+
+const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+  host: DB_HOST,
+  port: DB_PORT,
+  dialect: DB_DIALECT,
+  define: {
+    freezeTableName: true,
+  },
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+  logging: true,
+  dialectOptions: {
+    connectionTimeout: 60000,
+  },
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err);
+  });
+
+export default sequelize;
