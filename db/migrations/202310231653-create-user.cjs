cf1aae52abe87dcbed397294674628c47f8eec6b
Isaac Munene Ndereba <38168136+munenendereba@users.noreply.github.com>
2023-10-24 17:00:40 +0300

    user migration

---
+++ db/migrations/202310231653-create-user.cjs
@@ -0,0 +1,35 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("user", {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+      },
+      username: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      salt: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      status: {
+        type: Sequelize.ENUM,
+        values: ["active", "inactive", "disabled"],
+        allowNull: false,
+        defaultValue: "disabled",
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("user");
+  },
+};
