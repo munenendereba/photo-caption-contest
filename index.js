d67ce52cd4dc3327378dbc3bc45a7f12d3d345c4
Isaac Munene Ndereba <38168136+munenendereba@users.noreply.github.com>
2023-10-24 18:26:08 +0300

    expose post user

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import { configDotenv } from "dotenv";
 import morganBody from "morgan-body";
+import userMethods from "./controllers/user_controller.js";
 
 const app = express();
 configDotenv();
@@ -14,6 +15,8 @@ morganBody(app);
 app.get("/", (req, res) => {
   res.sendStatus(200);
 });
+
+app.post("/user", userMethods.createUser);
 
 const SERVER_PORT = process.env.SERVER_PORT || 3000;
 
