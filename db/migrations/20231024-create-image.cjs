a33189bfe64a2564bfadd0b2708fbdaf769ef22a
Isaac Munene Ndereba <38168136+munenendereba@users.noreply.github.com>
2023-10-24 19:30:17 +0300

    image model and migration

---
+++ db/migrations/20231024-create-image.cjs
@@ -0,0 +1,34 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("image", {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      path: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.fn("NOW"),
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.fn("NOW"),
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("image");
+  },
+};
---
+++ db/models/image.js
@@ -0,0 +1,18 @@
+import connection from "../config/connection.js";
+import Sequelize from "sequelize";
+
+const Image = connection.define("image", {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  path: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+});
