82f838cde7004fb2c48f20d50ad44917ece49538
Isaac Munene Ndereba <38168136+munenendereba@users.noreply.github.com>
2023-10-24 19:19:00 +0300

    all user methods

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,3 +1,4 @@
+import { response } from "express";
 import User from "../db/models/user.js";
 import passwordServices from "../services/password_services.js";
 
@@ -22,4 +23,80 @@ const createUser = async (request, response) => {
     });
 };
 
-export default { createUser };
+const getUser = (request, response) => {
+  const { username } = request.body;
+  const lowerUsername = username.toLowerCase();
+
+  User.findOne({ where: { username: lowerUsername } })
+    .then((user) => {
+      user
+        ? response.status(200).send(user)
+        : response.status(404).send("User not found.");
+    })
+    .catch((error) => {
+      response.status(500).send("An error occurred while getting user.");
+      console.log("Error getting user: ", error);
+    });
+};
+
+const updateUser = async (request, response) => {
+  const { username, status } = request.body;
+  const lowerUsername = username.toLowerCase();
+
+  const updatedUser = {
+    username: lowerUsername,
+    status: status,
+  };
+
+  User.update(updatedUser, { where: { username: lowerUsername } })
+    .then((user) => {
+      user
+        ? response.status(200)
+        : response.status(404).send("User not found.");
+    })
+    .catch((error) => {
+      response.status(500).send("An error occurred while updating user.");
+      console.log("Error updating user: ", error);
+    });
+};
+
+const changePassword = async (request, response) => {
+  const { username, password } = request.body;
+  const lowerUsername = username.toLowerCase();
+
+  const hashedPassword = await passwordServices.hashPassword(password);
+
+  const updatedUser = {
+    username: lowerUsername,
+    password: hashedPassword,
+  };
+
+  User.update(updatedUser, { where: { username: lowerUsername } })
+    .then((res) => {
+      res[0] === 1
+        ? response.status(200).send()
+        : response.status(404).send("User not found.");
+    })
+    .catch((error) => {
+      response.status(500).send("An error occurred while updating user.");
+      console.log("Error updating user: ", error);
+    });
+};
+
+const deleteUser = (request, response) => {
+  const { username } = request.body;
+  const lowerUsername = username.toLowerCase();
+
+  User.destroy({ where: { username: lowerUsername } })
+    .then((res) => {
+      res === 1
+        ? response.status(200).send()
+        : response.status(404).send("User not found.");
+    })
+    .catch((error) => {
+      response.status(500).send("An error occurred while deleting user.");
+      console.log("Error deleting user: ", error);
+    });
+};
+
+export default { createUser, getUser, updateUser, changePassword, deleteUser };
