e79d55a701b90ed1a6c9e11cd439ec30205ab50a
Isaac Munene Ndereba <38168136+munenendereba@users.noreply.github.com>
2023-10-27 19:46:06 +0300

    authentication completed

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ import { configDotenv } from "dotenv";
 import morganBody from "morgan-body";
 import userMethods from "./controllers/user_controller.js";
 import imageMethods from "./controllers/image_controller.js";
+import passport from "passport";
+import LocalStrategy from "passport-local";
+import session from "express-session";
+import { checkUserLoggedIn, authenticateUser } from "./services/auth.js";
 
 const app = express();
 configDotenv();
@@ -13,14 +17,55 @@ app.use(bodyParser.json());
 
 morganBody(app);
 
-app.get("/", (req, res) => {
-  res.sendStatus(200);
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.session());
+//userMethods.authenticateUser
+passport.use(new LocalStrategy(authenticateUser));
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+  })
+);
+
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
+
+app.get("/logout", (req, res, next) => {
+  req.logout((err) => {
+    if (err) return next(err);
+  });
+
+  res.redirect("/login");
+});
+
+app.get("/", checkUserLoggedIn, (req, res) => {
+  console.log("req.user: ", req.user.username);
+  res.render("index.ejs", { username: req.user.username });
 });
 
 app.post("/user", userMethods.createUser);
-app.post("/user/login", userMethods.userLogin);
+app.post("/user/login", userMethods.authenticateUser);
 app.get("/user/all", userMethods.getUsers);
-app.get("/user", userMethods.getUser);
+app.get("/user", userMethods.getUserByUsername);
 app.put("/user", userMethods.updateUser);
 app.put("/change-password", userMethods.changePassword);
 app.delete("/user", userMethods.deleteUser);
