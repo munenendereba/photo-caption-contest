9febed0a5a7f9a9cf80990358956072e27863eb5
Isaac Munene Ndereba <38168136+munenendereba@users.noreply.github.com>
2023-10-27 19:45:43 +0300

    auth service refactor

---
+++ services/auth.js
@@ -0,0 +1,32 @@
+import { comparePassword } from "./password_services.js";
+import User from "../db/models/user.js";
+
+export const checkUserLoggedIn = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+
+  res.redirect("/login");
+};
+
+export const authenticateUser = async (username, password, done) => {
+  try {
+    const lowerUsername = username.toLowerCase();
+    const user = await User.findOne({ where: { username: lowerUsername } });
+
+    if (!user) {
+      return done(null, false, { message: "Invalid user." });
+    }
+
+    const isPasswordValid = await comparePassword(password, user.password);
+
+    if (!isPasswordValid) {
+      return done(null, false, { message: "Invalid user." });
+    }
+
+    return done(null, user);
+  } catch (error) {
+    console.log("Error authenticating user: ", error);
+    return done(error);
+  }
+};
